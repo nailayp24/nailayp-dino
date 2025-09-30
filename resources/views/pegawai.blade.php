7dd9f7cb5cb611c3f1171364d68b427085149c99
nailayp24 <naila24si@mahasiswa.pcr.ac.id>
2025-09-30 08:02:54 +0700

    pert4

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -6,7 +6,7 @@
     <title>Pegawai Info</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
+<body> //halo
     <div class="container mt-4">
         <h1 class="mb-3">Data Pegawai</h1>
         <ul class="list-group">

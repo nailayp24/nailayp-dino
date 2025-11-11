838b54a6250e50790f50d516b9d1242b2b066a5f
nailayp24 <naila24si@mahasiswa.pcr.ac.id>
2025-11-11 10:01:19 +0700

    ubah sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -70,21 +70,7 @@
                     </a>
                 </li>
 
-                <!-- MENU PRODUCTS -->
-            <li class="nav-item {{ request()->routeIs('products.*') ? 'active' : '' }}">
-                <a href="{{ route('products.index') }}" class="nav-link">
-                    <span class="sidebar-icon">
-                        <svg class="icon icon-xs me-2" data-slot="icon" fill="none" stroke-width="1.5"
-                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z">
-                            </path>
-                        </svg>
-                    </span>
-                    <span class="sidebar-text">Products</span>
-                </a>
-            </li>
+            
                 <!-- Tambahkan blok kode ini -->
                 <li class="nav-item {{ request()->routeIs('pelanggan.*') ? 'active' : '' }}">
                     <a href="{{ route('pelanggan.index') }}" class="nav-link">
